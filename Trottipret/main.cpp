9cf0dbefaeed327f8f451fec63e3568a96b1d1c9
robergem2u <robergem2u@etu.univ-lorraine.fr>
2019-12-03 14:51:36 +0100

    ajout des methodes de connexion.cpp

--- Trottipret/main.cpp
+++ Trottipret/main.cpp
@@ -1,5 +1,6 @@
 #include "gestionnaireCompte.h"
 #include "inscription.h"
+#include "connexion.h"
 #include <QApplication>
 
 using namespace std;
@@ -7,8 +8,9 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Inscription inscrpt;
-    inscrpt.show();
+
+    Connexion connect;
+    connect.show();
 
     return a.exec();
 }
